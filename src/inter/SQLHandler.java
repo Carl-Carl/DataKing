1c20c18f57676273e1fe8bd78a997c07507e6260
Snowball <447324663@qq.com>
2020-12-22 11:55:42 +0800

    1

--- src/inter/SQLHandler.java
+++ src/inter/SQLHandler.java
@@ -10,10 +10,14 @@ package inter;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.util.ArrayList;
+
 import sql.Parser;
 import sql.Request;
 import core.*;
 import core.sql.*;
+
+import javax.print.DocFlavor;
 
 /**
  * The universal class to handle queries to the database
@@ -46,15 +50,63 @@ public class SQLHandler {
     private static final Query select = new Query() {
         @Override
         public void query(Request request) {
-            Statement a = new Statement();
-            
+
         }
     };
 
     private static final Query create = new Query() {
         @Override
         public void query(Request request) {
-            
+
+            Statement sta = new Statement();
+            String[] table = request.getFrom();
+            for (Pack pack : sta.packs) {
+                if (table[0].equals(pack.getTable())){
+                    /*
+                    * create table重复的异常
+                    * */
+                    System.out.println(table[0]+"already exists!\n");
+                    return;
+                }
+            }
+
+            String[] temp = request.getSet();
+            ArrayList<String> name_ = new ArrayList<String>();
+            ArrayList<Class<?>> columns_ = new ArrayList<Class<?>>();
+            for (String s : temp){
+                String[] split = s.split("=");
+                if (split.length != 2){
+                    /*
+                    * set 输入异常
+                    * */
+                    System.out.println("Wrong SQL expression!\n");
+                    return;
+                }
+                name_.add(split[0].toString());
+                if (split[1].equals("String")){
+                    columns_.add(String.class);
+                }if (split[1].equals("Integer")){
+                    columns_.add(Integer.class);
+                }if (split[1].equals("Double")){
+                    columns_.add(Double.class);
+                }
+            }
+
+            int size_of_name = name_.size();
+            int size_of_columns = columns_.size();
+            String[] name = (String[]) name_.toArray(new String[size_of_name]);
+            Class<?>[] columns = (Class<?>[]) columns_.toArray(new Class<?>[size_of_columns]);
+            int i;
+            for (i = 0; i < 10; i++) {
+                if(sta.packs[i] == null){
+                    try {
+                        sta.packs[i] = new Pack(table[0], name, columns);
+                        break;
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
         }
     };
 
@@ -69,6 +121,39 @@ public class SQLHandler {
         @Override
         public void query(Request request) {
 
+            Statement sta = new Statement();
+            String[] table = request.getFrom();
+            for (Pack pack : sta.packs) {
+                if (table[0].equals(pack.getTable())){
+                    if (request.getSet() == null){
+                        /*
+                        * 此处没有检查主关键字值是否相同
+                        * */
+                        pack.add(request.getValues());
+                    }
+                    else {
+                        String[] name = request.getSet();
+                        ArrayList<Object> item_new = new ArrayList<Object>();
+                        /*
+                        * 表头列表进行对应
+                        * */
+
+                        int size = item_new.size();
+                        Object[] item = (Object[])item_new.toArray(new Object[size]);
+                        pack.add(item);
+                        /*
+                        * 此处不检查主关键字是否相同
+                        * 加入后提示成功
+                        * */
+                        System.out.println("Insert item succeed!\n");
+                    }
+                    return;
+                }
+            }
+            /*
+            * 无用以插入新值的table
+            * */
+            System.out.println("No table exist!\n");
         }
     };
 
@@ -82,7 +167,22 @@ public class SQLHandler {
     private static final Query drop = new Query() {
         @Override
         public void query(Request request) {
-
+            Statement sta = new Statement();
+            String[] table = request.getFrom();
+            for (int i = 0; i < sta.packs.length; i++) {
+                if(table[0].equals(sta.packs[i].getTable())){
+                    sta.packs[i] = null;
+                    /*
+                    * 删除原有的pack
+                    * */
+                    System.out.println(table[0]+"drop success!\n");
+                    break;
+                }
+            }
+            /*
+            * 没有要删除的表
+            * */
+            System.out.println(table[0]+"doesn't exist!\n");
         }
     };
 
