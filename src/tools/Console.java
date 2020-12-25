4ccf3adfd79da67dd47a396334bac1c86062951c
Carl-Carl <61238494+Carl-Carl@users.noreply.github.com>
2020-12-25 15:22:20 +0800

    1

--- src/tools/Console.java
+++ src/tools/Console.java
@@ -1,7 +1,7 @@
 /*
 * @Author: your name
 * @Date: 2020-12-25 14:51:55
- * @LastEditTime: 2020-12-25 15:02:49
+ * @LastEditTime: 2020-12-25 15:22:01
  * @LastEditors: Please set LastEditors
 * @Description: In User Settings Edit
 * @FilePath: \DataKing\src\tools\Console.java
@@ -13,6 +13,7 @@ import java.util.Scanner;
 
 import inter.Connection;
 import inter.DriverManager;
+import inter.Statement;
 
 public class Console {
 
@@ -28,14 +29,27 @@ public class Console {
             if (con == null)
                 continue;
 
+            Statement st = con.getStatement();
             System.out.println("Open database \""+ dbName + "\" successfully.");
             System.out.println("Please input SQL orders.");
             System.out.println("Input \"exit\" to close.");
 
             while (true) {
                 String sql = sc.nextLine().trim();
+                if (sql.equalsIgnoreCase("exit")) {
+                    sc.close();
+                    return;
+                }
+                
+                var res = st.executeQuery(sql);
+                if (res != null)
+                    res.print();
+                else if (st.executeUpdate(sql))
+                    System.out.println("Execute successfully.");
+                else
+                    System.out.println("Invalid input.");
+                
             }
         }
-        
     }
 }
